c3a1a175b393a1398ec8ddaa3a683341baaa5a87
Arlisson N. Costa <arlisson314@gmail.com>
2023-02-17 15:18:18 -0300

    feat: ✅ implementa o método get_most_ordered_dish_per_customer que  retorna o prato mais pedido

--- src/track_orders.py
+++ src/track_orders.py
@@ -7,10 +7,23 @@ class TrackOrders:
         return len(self.orders)
 
     def add_new_order(self, customer, order, day):
-        pass
+        self.orders.append((customer, order, day))
 
     def get_most_ordered_dish_per_customer(self, customer):
-        pass
+        f_dish = [line[1] for line in self.orders if line[0] == customer]
+        count = {}
+        most_requested = f_dish[0]
+
+        for dish in f_dish:
+            if dish in count:
+                count[dish] += 1
+            else:
+                count[dish] = 1
+
+        if count[dish] > count[most_requested]:
+            most_requested = dish
+
+        return most_requested
 
     def get_never_ordered_per_customer(self, customer):
         pass
